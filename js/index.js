1387ebe14286d2ec91d47784ab6e6231c9b2e75c
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-13 11:49:05 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -4,10 +4,16 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+addButton.addEventListener("click", (event) => {
+  
+event.preventDefault();
+const newToastMessage = document.createElement("li"); // wir nehmen das createElement und packen es in eine neue Variable
+newToastMessage.classList.add("toast-container__message"); // wir nehmen die Variable und fügen die Class Toast-container_message hinzu
+newToastMessage.textContent="ich bin eine neue Toast Message"; // 
+toastContainer.append(newToastMessage); // Setzt den Textinhalt der neuen Karte auf den Wert, der gerade in der Texteingabe steht.
+console.log(newToastMessage); // funktioniert alles? Testen in der Konsole
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+ toastContainer.innerHTML = "";
 });
